8fe6d9d8b01640d518f0a8782f6bb7185d21c29a
Oterea <148307377+Oterea@users.noreply.github.com>
2025-01-31 00:10:30 +0800

    Update get.sh

--- get.sh
+++ get.sh
@@ -1,4 +1,4 @@
-curl -L -o sb.sh https://gh-proxy.com/https://raw.githubusercontent.com/Oterea/sing-box-shell/main/sb.sh
+curl -L -o sb.sh https://raw.githubusercontent.com/Oterea/sing-box-shell/main/sb.sh
 chmod +x sb.sh
 
 
