1d196e2ec03e118763d57b653d13ee51ae8b52ce
Oterea <hyczsin@gmail.com>
2025-02-06 11:24:06 +0800

    fixes

--- get.sh
+++ get.sh
@@ -6,7 +6,7 @@ remove_sb() {
     echo
     echo -e "${GREEN}INFO: Old sing-box removed successfully.${RESET}"
 }
-
+remove_sb
 curl -L -o sb.sh https://github.oterea.top/https://raw.githubusercontent.com/Oterea/sing-box-shell/main/sb.sh
 sudo chmod +x sb.sh
 
