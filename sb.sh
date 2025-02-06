fa6cef82da93cd381efe5d651bc62c9e54440e74
Oterea <oterea@Otereas-Mac-mini.local>
2025-02-06 10:48:38 +0800

    fixes

--- sb.sh
+++ sb.sh
@@ -28,6 +28,90 @@ else
     exit
 fi
 
+
+install_sb() {
+    echo -e "${PURPLE}============================================${RESET}"
+    echo -e "${PURPLE}              Updating sing-box             ${RESET}"
+
+    echo -e "${CYAN}默认下载链接: $SB_URL${RESET}"
+    echo -e "${CYAN}是否使用默认下载链接([Y]/n): ${RESET}"
+    read sub_choice
+    sub_choice=${sub_choice:-y}
+
+    # 转换为小写并使用 if 语句判断
+    if [[ "${sub_choice,,}" == "y" ]]; then
+        :
+        # 在这里执行使用默认链接的操作
+    elif [[ "${sub_choice,,}" == "n" ]]; then
+        # 在这里执行不使用默认链接的操作
+        echo -e "${CYAN}请输入 sing-box 下载链接: ${RESET}"
+        read sb_url
+        # 检查 url.txt 是否已经有 SB_URL，如果有则替换，否则追加
+        if grep -q '^SB_URL=' $URL; then
+            # 替换已有的 SB_URL
+            sed -i 's|^SB_URL=.*|SB_URL="'"$PROXY/$sb_url"'"|' $URL
+        else
+            # 追加新变量到 url.txt
+            echo "SB_URL=\"$PROXY/$sb_url\"" >> $URL
+        fi
+
+    else
+        echo -e "${YELLOW}WARN: 无效的选择，请输入 y 或 n${RESET}"
+    fi
+
+    source $URL
+    FILE_NAME=$(basename "$SB_URL")
+
+    success=1
+    # curl 下载
+    
+    echo -e "${GREEN}INFO: Using curl to download the file...${RESET}"
+    curl -o "$TARGET_DIR/$FILE_NAME" -L "$SB_URL"
+    if [ $? -eq 0 ]; then
+        success=0
+    fi
+
+
+    # 检查下载是否成功
+    if [ "$success" -eq 0 ]; then
+        echo -e "${GREEN}INFO: sing-box downloaded successfully to $TARGET_DIR/$FILE_NAME${RESET}"
+    else
+        echo -e "${RED}ERROE: File download failed.${RESET}"
+        rm $TARGET_DIR/$FILE_NAME
+        break
+    fi
+
+    # 检查解压工具 tar 是否安装，如果没有则自动安装
+    if ! command -v tar >/dev/null 2>&1; then
+        echo -e "${YELLOW}WARN: tar is not installed. Installing tar...${RESET}"
+        sudo apt update && sudo apt install -y tar
+        if [ $? -ne 0 ]; then
+            echo -e "${RED}ERROR: Failed to install tar. Exiting...${RESET}"
+            break
+        fi
+    fi
+
+    # 解压并提取内容到目标目录
+    tar --strip-components=1 -xzf "$TARGET_DIR/$FILE_NAME" -C "$EXTRACT_DIR"
+    if [ $? -eq 0 ]; then
+        echo -e "${GREEN}INFO: ${FILE_NAME} extracted successfully to $EXTRACT_DIR${RESET}"
+    else
+        echo -e "${RED}ERROR: Failed to extract sing-box.${RESET}"
+        break
+    fi
+    # 删除源文件
+    rm "$TARGET_DIR/$FILE_NAME"
+
+    # 提取版本信息
+    VERSION_DATA=$($EXTRACT_DIR/sing-box version)
+    VERSION=$(echo "$VERSION_DATA" | grep -oP 'sing-box version \K[0-9]+\.[0-9]+\.[0-9]+')
+    VERSION_INFO="sing-box-$VERSION"
+
+
+
+    echo "Hello from my function!"
+}
+
 # 一级菜单
 while true; do
 
@@ -52,84 +136,7 @@ while true; do
     case $choice in
 
         1)
-            echo -e "${PURPLE}============================================${RESET}"
-            echo -e "${PURPLE}              Updating sing-box             ${RESET}"
-
-            echo -e "${CYAN}默认下载链接: $SB_URL${RESET}"
-            echo -e "${CYAN}是否使用默认下载链接([Y]/n): ${RESET}"
-            read sub_choice
-            sub_choice=${sub_choice:-y}
-
-            # 转换为小写并使用 if 语句判断
-            if [[ "${sub_choice,,}" == "y" ]]; then
-                :
-                # 在这里执行使用默认链接的操作
-            elif [[ "${sub_choice,,}" == "n" ]]; then
-                # 在这里执行不使用默认链接的操作
-                echo -e "${CYAN}请输入 sing-box 下载链接: ${RESET}"
-                read sb_url
-                # 检查 url.txt 是否已经有 SB_URL，如果有则替换，否则追加
-                if grep -q '^SB_URL=' $URL; then
-                    # 替换已有的 SB_URL
-                    sed -i 's|^SB_URL=.*|SB_URL="'"$PROXY/$sb_url"'"|' $URL
-                else
-                    # 追加新变量到 url.txt
-                    echo "SB_URL=\"$PROXY/$sb_url\"" >> $URL
-                fi
-
-            else
-                echo -e "${YELLOW}WARN: 无效的选择，请输入 y 或 n${RESET}"
-            fi
-
-            source $URL
-            FILE_NAME=$(basename "$SB_URL")
-
-            success=1
-            # curl 下载
-         
-            echo -e "${GREEN}INFO: Using curl to download the file...${RESET}"
-            curl -o "$TARGET_DIR/$FILE_NAME" -L "$SB_URL"
-            if [ $? -eq 0 ]; then
-                success=0
-            fi
-    
-
-            # 检查下载是否成功
-            if [ "$success" -eq 0 ]; then
-                echo -e "${GREEN}INFO: sing-box downloaded successfully to $TARGET_DIR/$FILE_NAME${RESET}"
-            else
-                echo -e "${RED}ERROE: File download failed.${RESET}"
-                rm $TARGET_DIR/$FILE_NAME
-                break
-            fi
-
-            # 检查解压工具 tar 是否安装，如果没有则自动安装
-            if ! command -v tar >/dev/null 2>&1; then
-                echo -e "${YELLOW}WARN: tar is not installed. Installing tar...${RESET}"
-                sudo apt update && sudo apt install -y tar
-                if [ $? -ne 0 ]; then
-                    echo -e "${RED}ERROR: Failed to install tar. Exiting...${RESET}"
-                    break
-                fi
-            fi
-
-            # 解压并提取内容到目标目录
-            tar --strip-components=1 -xzf "$TARGET_DIR/$FILE_NAME" -C "$EXTRACT_DIR"
-            if [ $? -eq 0 ]; then
-                echo -e "${GREEN}INFO: ${FILE_NAME} extracted successfully to $EXTRACT_DIR${RESET}"
-            else
-                echo -e "${RED}ERROR: Failed to extract sing-box.${RESET}"
-                break
-            fi
-            # 删除源文件
-            rm "$TARGET_DIR/$FILE_NAME"
-
-            # 提取版本信息
-            VERSION_DATA=$($EXTRACT_DIR/sing-box version)
-            VERSION=$(echo "$VERSION_DATA" | grep -oP 'sing-box version \K[0-9]+\.[0-9]+\.[0-9]+')
-            VERSION_INFO="sing-box-$VERSION"
-
-
+            install_sb
 
             ;;
         2)
@@ -224,6 +231,7 @@ while true; do
         5)  
             sudo rm -r $EXTRACT_DIR
             sudo rm /etc/systemd/system/sb.service
+            sudo rm /usr/local/bin/sb
             echo
             echo -e "${GREEN}INFO: sing-box removed successfully.${RESET}"
             break
