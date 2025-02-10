861db2ed6126d50bc4b395a6bbb1de236e808b28
Oterea <hyczsin@gmail.com>
2025-02-10 11:50:46 +0800

    remove bold

--- sb.sh
+++ sb.sh
@@ -28,7 +28,7 @@ if [ ! -d "$work_dir" ]; then
     mkdir -p "$work_dir"
 fi
 info() {
-    printf '%s\n' "${BOLD}${GREY}>${RESET} ${GREEN}INFO:${RESET} $*"
+    printf '%s\n' "${GREY}>${RESET} ${GREEN}INFO:${RESET} $*"
 }
 warn() {
     printf '%s\n' "${BOLD}${GREY}>${RESET} ${YELLOW}WARN:${RESET} $*"
